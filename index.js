1b9f8b6d1df8510f51624362c5218b5274a6201f
Aleksandr Karibov <karibov3194@gmail.com>
2024-04-04 22:09:19 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+import getArgumentDefaultValue from "./src/getArgumentDefaultValue";
+import getClassConstructorArgsNames from "./src/getClassConstructorArgsNames";
+import getFunctionArgsNames from "./src/getFunctionArgsNames";
+import getModuleImports from "./src/getModuleImports";
+import parseType from "./src/parseType";
+
+export default {
+    getArgumentDefaultValue,
+    getClassConstructorArgsNames,
+    getFunctionArgsNames,
+    getModuleImports,
+    parseType,
+};
