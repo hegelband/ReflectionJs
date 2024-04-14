e5ea29def0df96cbc120a48cba25fb747a74a578
Aleksandr Karibov <karibov3194@gmail.com>
2024-04-14 23:54:46 +0300

    fix exports

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import getArgumentDefaultValue from "./src/getArgumentDefaultValue";
-import getClassConstructorArgsNames from "./src/getClassConstructorArgsNames";
-import getFunctionArgsNames from "./src/getFunctionArgsNames";
-import getModuleImports from "./src/getModuleImports";
-import parseType from "./src/parseType";
+import argDefaultValue from "./src/getArgumentDefaultValue";
+import classConstructorArgsNames from "./src/getClassConstructorArgsNames";
+import functionArgsNames from "./src/getFunctionArgsNames";
+import moduleImports from "./src/getModuleImports";
+import getType from "./src/parseType";
 
-export const getArgumentDefaultValue = getArgumentDefaultValue;
-export const getClassConstructorArgsNames = getClassConstructorArgsNames;
-export const getFunctionArgsNames = getFunctionArgsNames;
-export const getModuleImports = getModuleImports;
-export const parseType = parseType;
+export const getArgumentDefaultValue = argDefaultValue;
+export const getClassConstructorArgsNames = classConstructorArgsNames;
+export const getFunctionArgsNames = functionArgsNames;
+export const getModuleImports = moduleImports;
+export const parseType = getType;
+
+export default {
+    getArgumentDefaultValue,
+    getClassConstructorArgsNames,
+    getFunctionArgsNames,
+    getModuleImports,
+    parseType,
+};
