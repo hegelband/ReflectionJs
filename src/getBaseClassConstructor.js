70e86d4f09ac569caaaf831fdc256c26732c3952
Aleksandr Karibov <karibov3194@gmail.com>
2024-04-16 21:49:49 +0300

    fix getBaseClass

--- src/getBaseClassConstructor.js
+++ src/getBaseClassConstructor.js
@@ -1,8 +1,5 @@
 const getBaseClassConstructor = (cls) => {
     const constructor = cls.prototype.__proto__.constructor;
-    if (constructor.name === 'Object') {
-        return Object;
-    }
     return constructor;
 };
 
