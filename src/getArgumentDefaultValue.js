220929e390e3d4dcfeee97dac2283d750c036f59
Aleksandr Karibov <karibov3194@gmail.com>
2024-04-17 22:48:53 +0300

    getStringifiedEntityName

--- src/getArgumentDefaultValue.js
+++ src/getArgumentDefaultValue.js
@@ -1,10 +1,12 @@
+import getStringifiedEntityName from "./getStringifiedEntityName";
+
 function getArgumentDefaultValue(arg) {
     if (!arg) return null;
     const parts = arg.split('=');
     if (parts.length > 1) {
         return {
             name: parts[0].trim(),
-            value: parts[1].trim(),
+            value: getStringifiedEntityName(parts[1].trim()),
         };
     }
     return null;
---
+++ src/getStringifiedEntityName.js
@@ -0,0 +1,10 @@
+const getStringifiedEntityName = (entityStr) => {
+    if (entityStr.includes('WEBPACK_IMPORTED_MODULE')) {
+        const startIndex = entityStr.indexOf('_');
+        const endIndex = entityStr.indexOf('_', startIndex + 1);
+        return entityStr.slice(startIndex, endIndex);
+    }
+    return entityStr;
+};
+
+export default getStringifiedEntityName;
